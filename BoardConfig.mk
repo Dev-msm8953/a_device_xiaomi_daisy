geopd/device_xiaomi_sakura

commit ea6f1987e7bec633f874879ae1a20d2c68577618

sakura: Flatten APEXs for performance
APEXs have quite a bit of overhead in updatable form: they are ext4
filesystems mounted using loopback block devices, stored on yet another
ext4 filesystem (system or vendor). They also increase boot time
substantially due to needing to set up and mount 27 images with AVB. On
a Pixel 2 XL, apexd-bootstrap took 485 ms during boot, and apexd took
471 ms for a total of 955 ms — nearly a second — spent on APEX setup,
which is quite significant for modern devices. At least 422 ms out of
that was spent blocking the rest of the system's initialization, but the
overall slowdown is likely greater due to increased system load.

Updatable APEXs likely also come with a non-negligible amount of memory
overhead due to setting up and mounting so many filesystem images, as
well as AVB and potential double-caching effects caused by having two
layers of filesystems in use.

Being able to update APEXs is useless for us anyway because we don't
use Google's prebuilt modules and we have no other way to deliver APEX
updates. Even if we were able to use Google's modules, we still want to
be able to modify many of the APEX modules, so we need to build them
from source.

Flattened APEXs have negligible overhead, since they're just stored as
directories in /<partition>/apex and bind-mounted to /apex/<pkg name>.

Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

BoardConfig.mk
@@ -40,6 +40,7 @@ TARGET_BOARD_SUFFIX := _64
 
 # APEX image
 DEXPREOPT_GENERATE_APEX_IMAGE := true
+OVERRIDE_TARGET_FLATTEN_APEX := true
 
 # Audio
 AUDIO_FEATURE_ENABLED_ANC_HEADSET := true

device.mk
@@ -18,9 +18,6 @@ $(call inherit-product, vendor/xiaomi/sakura/sakura-vendor.mk)
 $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \

init/init_msm8953.cpp
@@ -137,7 +137,7 @@ void vendor_load_properties() {
     property_override_multifp("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "ro.bootimage.build.fingerprint", "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys");
 
     // Misc
-    property_override("ro.apex.updatable", "true");
+    property_override("ro.apex.updatable", "false");
     property_override("ro.oem_unlock_supported", "0");
     property_override("ro.com.google.clientidbase", "android-xiaomi");
     property_override("ro.com.google.clientidbase.ms", "android-xiaomi-rev1");

Dev-msm8953/a_device_xiaomi_daisy

commit 173a87c5642f1344ccf8b81efff410f476c69cb1

daisy: Remove Flatten APEX, ro.apex.updatable and GenerateApexImage
- References:
https://android-review.googlesource.com/c/platform/system/apex/+/2636909
https://android-review.googlesource.com/c/platform/cts/+/2745258
https://android-review.googlesource.com/c/platform/system/vold/+/2727653
https://android-review.googlesource.com/c/platform/build/+/1230120
https://source.android.com/devices/tech/ota/apex#kernel-requirements
https://source.android.com/devices/architecture/modular-system#architecture
https://github.com/piscesmk2013/device_oneplus_sm8150-common/commit/c87dd280fd908795e9e9c4b80c6fd0827862b684

- Don't explicitly include updatable_apex.mk
It is inherited by base_system.mk as of QPR2

This fixes: ea6f1987e7bec633f874879ae1a20d2c68577618, 2b946c490537011655300c59c64eb397d4836f3b, e274a0b694196785983de44ed4e8c8acec68dbe7.

Change-Id: a5dacb652abc1ed0bb7bbbd6a1b70a9632eda409
Signed-off-by: TogoFire <togofire@mailfence.com>

BoardConfig.mk
@@ -38,10 +38,6 @@ BUILD_BROKEN_DUP_RULES := true
 
 TARGET_BOARD_SUFFIX := _64
 
-# APEX image
-DEXPREOPT_GENERATE_APEX_IMAGE := true
-OVERRIDE_TARGET_FLATTEN_APEX := true
-
 # Audio
 AUDIO_FEATURE_ENABLED_ANC_HEADSET := true
 AUDIO_FEATURE_ENABLED_ALAC_OFFLOAD := true

init/init_daisy.cpp
@@ -118,7 +118,6 @@ void vendor_load_properties() {
     workaround_properties();
 
     // Misc
-    property_override("ro.apex.updatable", "false");
     property_override("ro.iorapd.enable", "false");
     property_override("ro.oem_unlock_supported", "0");
     property_override("ro.com.google.clientidbase", "android-xiaomi");